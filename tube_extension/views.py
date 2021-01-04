8c8abf08f671656f57f29ccdd8ca6da982fe6a35
Your Name <you@example.com>
2021-01-04 00:30:27 +0000

    views renewed

--- tube_extension/views.py
+++ tube_extension/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView, CreateView
 
 from tube_extension.models import Video
 from tube_extension.forms import VideoForm
 
 
-class VideoFormView(FormView):
+class AormView(FormView):
     template_name = "tube_extension/video_list.html"
     form_class = VideoForm
     success_url = "/"
@@ -21,6 +21,30 @@ class VideoFormView(FormView):
     
     def form_valid(self,form):
         form.save(self.request)
+        return super().form_valid(form)
+
+class VideoFormView(CreateView):
+    template_name = "tube_extension/video_list.html"
+    model=Video
+    fields = ['title','link']
+    success_url = "/"
+    
+
+    
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['videos'] = Video.objects.filter(user=self.request.user)
+        return ctx
+        
+     
+    def form_valid(self,form):   
+        video = form.save()
+        video.title = self.request.POST.get('title')
+        link = self.request.POST.get('link')
+        video.link = link.split("v=")[1].split("&")[0]
+        video.user = self.request.user
+        #print(video_id)
+        video.save()
         return super().form_valid(form)
     
 class WatchVideoView(DetailView):
